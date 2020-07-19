41141fda72a32565a8ea7094315662196b69d9c9
Ashish Pathak <pathak.ashish87@gmail.com>
2020-07-20 01:01:09 +0530

    Make OkHttp related instances singleton

--- app/src/main/java/com/ashish/imgursearchdemo/di/OkHttpModule.kt
+++ app/src/main/java/com/ashish/imgursearchdemo/di/OkHttpModule.kt
@@ -7,12 +7,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object OkHttpModule {
 
     @Provides
+    @Singleton
     fun provideAuthInterceptorOkHttpClient(
         authInterceptor: AuthInterceptor,
         loggingInterceptor: HttpLoggingInterceptor
@@ -24,11 +26,13 @@ object OkHttpModule {
     }
 
     @Provides
+    @Singleton
     fun provideAuthInterceptor(): AuthInterceptor {
         return AuthInterceptor()
     }
 
     @Provides
+    @Singleton
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().apply {
             // TODO Change this to NONE based on release type
