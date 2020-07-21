42ee61367000a9cf2d29ad48506493a4e6918139
Ashish Pathak <pathak.ashish87@gmail.com>
2020-07-21 14:56:09 +0530

    Remove IDE created Activities and add blank SearchActivity

---
+++ app/src/main/java/com/ashish/imgursearchdemo/ui/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.ashish.imgursearchdemo.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ashish.imgursearchdemo.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
